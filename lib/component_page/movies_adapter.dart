85e5a514e7ac78ea6c31ac64f4377b6fa198ffb9
wisnuibroo <wisnuaja0999@gmai.com>
2024-09-30 10:00:13 +0700

    update-movies-adapter

--- lib/component_page/movies_adapter.dart
+++ lib/component_page/movies_adapter.dart
@@ -3,73 +3,40 @@ import 'package:tugas_state_management_getx_movies/model/model_movies.dart';
 
 class MovieAdapter extends StatelessWidget {
   final List<ModelMovies> movies;
+  final double posterWidth;
+  final double posterHeight;
 
   const MovieAdapter({
     Key? key,
     required this.movies,
+    required this.posterWidth,
+    required this.posterHeight,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: movies.length,
-      itemBuilder: (context, index) {
-        final movie = movies[index];
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-          child: Card(
-            color: const Color(0xFF383838),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+    return SizedBox(
+      height: posterHeight,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          final movie = movies[index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              children: [
+                Image.asset(
+                  movie.imagePath,
+                  width: 120,
+                  height: 180,
+                  fit: BoxFit.cover,
+                ),
+              ],
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: Image.asset(
-                      movie.imagePath,
-                      width: 100,
-                      height: 150,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Container(
-                          width: 100,
-                          height: 150,
-                          color: Colors.grey,
-                          child: const Icon(
-                            Icons.image_not_supported,
-                            color: Colors.white,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          movie.title,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
